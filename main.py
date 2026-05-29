73028562f752a720fbf06d16babcd7569d8e04ba
YasminDreer <yasmimdreer_@hotmail.com>
2026-05-29 12:47:00 +0100

    Final do sprint
    Atualizado conforme a categoria da OMS

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ imc = peso / (altura_m * altura_m)
 print(f"\nIMC: {imc:.2f}")
 
 if imc < 18.5:
-    print("Fora do peso normal")
-elif imc < 25:
+    print("Abaixo do peso")
+elif imc >= 18.5 and imc <25:
     print("Peso normal")
-else:
-    print("Fora do peso normal")
+elif imc >= 25 and imc < 30:
+    print("Excesso de Peso")
+elif imc >= 30:
+    print("Obesidade")
